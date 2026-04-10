6552ff7fded128bd9e7fdce2d0707be573bc541f
SaadiaAjbbar <ajbbarsaa6@gmail.com>
2026-04-10 21:28:41 +0100

    creation de model Question.php

--- backend/src/app/Models/Question.php
+++ backend/src/app/Models/Question.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    
     protected $fillable = ['titre'];
     public function medecin(){
         return $this->belongsTo(Medecin::class);
