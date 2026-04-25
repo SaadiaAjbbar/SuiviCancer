51308c232bf98f00140930c3d736a5a0d1c1d29e
SaadiaAjbbar <ajbbarsaa6@gmail.com>
2026-04-25 10:32:53 +0100

    feat: API CRUD pour les toxicités et symptômes (Hospital Admin)

--- backend/src/app/Http/Controllers/SymptomeController.php
+++ backend/src/app/Http/Controllers/SymptomeController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Symptome;
 
 class SymptomeController extends Controller
+
 {
     public function store(Request $request)
     {
--- backend/src/app/Http/Controllers/ToxiciteController.php
+++ backend/src/app/Http/Controllers/ToxiciteController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 
 class ToxiciteController extends Controller
 {
+    
     public function store(Request $request)
     {
         // 1. Validation
